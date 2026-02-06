bc0dc1eeba211dfb21339ef8be1797b1f7faa71f
karolysovagoozeki <85869266+karolysovagoozeki@users.noreply.github.com>
2026-02-06 14:25:06 +0100

    Create ozekiaigateway.rb
    Add ozekiaigateway formula

---
+++ Formula/ozekiaigateway.rb
@@ -0,0 +1,40 @@
+class Ozekiaigateway < Formula
+  desc "Ozeki AI Gateway web files for Apache/PHP"
+  homepage "https://ozeki.hu"
+  version "1.0.2"
+  url "https://repo.ozeki.hu/pool/main/o/ozekiaigateway/ozekiaigateway_1.0.2.tar"
+  
+  sha256 "302efb531cfd194e6362dadf174cadb3d5199cfa68bcdf364f22f53d5d2ca897"
+
+  depends_on "httpd"
+  depends_on "php"
+
+  def install
+    
+    www_dir = "#{HOMEBREW_PREFIX}/var/www"
+    system "tar", "xf", "#{@downloaded_file}", "--strip-components=0", "--directory=#{www_dir}"
+    
+  end
+
+  def post_install
+    www_dir = "#{HOMEBREW_PREFIX}/var/www"
+    
+    (www_dir).mkpath unless Dir.exist?(www_dir)
+    
+    chmod 0755, www_dir
+    chown ENV["USER"], OS.mac? ? "_www" : "www-data", www_dir, recursive: true
+    
+    system "#{HOMEBREW_PREFIX}/bin/brew", "services", "restart", "httpd" if service_running?("httpd")
+    system "#{HOMEBREW_PREFIX}/bin/brew", "services", "restart", "php" if service_running?("php")
+  end
+
+  def service_running?(name)
+    system "brew", "services", "list", name, "|", "grep", "^#{name}.*started"
+  end
+
+  test do
+    www_dir = "#{HOMEBREW_PREFIX}/var/www"
+    assert_predicate Pathname(www_dir), :exist?, "WWW dir created"
+    assert_predicate Pathname("#{www_dir}/index.php"), :exist?, "Web files installed"  # Ellenőrizd a tar tartalmát
+  end
+end
